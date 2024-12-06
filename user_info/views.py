ba20bb4acf0dbef6132903328dff0c10e773dde6
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2024-12-06 14:50:18 +0200

    url and view changes

--- user_info/views.py
+++ user_info/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from .forms import CustomUserCreationForm
+
 
 def home_page(request):
     return render(request, 'home.html')
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('home')
